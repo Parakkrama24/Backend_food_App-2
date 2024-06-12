92bbba0815e1306e7bb1d8ff138303b95738c401
Parakkrama Dasanayaka <110714055+Parakkrama24@users.noreply.github.com>
2024-06-12 14:37:06 +0530

    Create CategoryController.java

---
+++ Backend_food_App-3/src/main/java/com/Dasanayaka/Backend/controller/CategoryController.java
@@ -0,0 +1,33 @@
+package com.Dasanayaka.Backend.controller;
+
+
+import com.Dasanayaka.Backend.model.Catagory;
+import com.Dasanayaka.Backend.model.User;
+import com.Dasanayaka.Backend.service.CategoryService;
+import com.Dasanayaka.Backend.service.Userservice;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/admin/category")
+public class CategoryController {
+
+    @Autowired
+    private CategoryService categoryService;
+
+    @Autowired
+    private Userservice userservice;
+
+    public ResponseEntity<Catagory> createCategory(@RequestBody Catagory catagory,
+                                                   @RequestHeader("Authorization") String jwt) throws Exception {
+        User user   =userservice.findUsserByJwtToken(jwt);
+        Catagory createdCategory= categoryService.CreateCategory(catagory.getName(),user.getId());
+        return  new ResponseEntity<>(createdCategory, HttpStatus.CREATED);
+
+    }
+}
